9bb859a39243c9aac4061d344ed240f6c9be6487
Leannelb <leannelaceybyrne@outlook.com>
2019-01-09 19:22:18 +0100

    using HostListener and HostBinding to dynamically change style of element ona  a web page

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,28 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  Renderer2,
+  HostListener,
+  HostBinding,
+ } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+   
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.backgroundColor = 'blue';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.backgroundColor = 'transparent';
   }
 }
